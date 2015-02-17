abcaba797a0293d9343a91eef2ff760edbd5ee7c
LauraBogatu <laurabogatu@yahoo.com>
2015-02-17 20:39:30 +0000

    Complete main
    Added some code for reference that has everything in the same place.
    The libraries for usb don’t work properly.

---
+++ main_with_dash_and_logging.h
@@ -0,0 +1,14 @@
+//
+//  main_with_dash_and_logging.h
+//  
+//
+//  Created by Laura on 17/02/15.
+//
+//
+
+#ifndef ____main_with_dash_and_logging__
+#define ____main_with_dash_and_logging__
+
+#include <stdio.h>
+
+#endif /* defined(____main_with_dash_and_logging__) */
